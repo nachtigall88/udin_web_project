ce053f4a22bc324fe86f332d3cf8ba6a99cc159e
nachtigall88 <ud1narty0m123@gmail.com>
2023-07-25 14:47:09 +0300

    task_4_4.py

--- exercises/04_data_structures/task_4_3.py
+++ exercises/04_data_structures/task_4_3.py
@@ -18,3 +18,7 @@
 """
 
 config = "switchport trunk allowed vlan 1,3,10,20,30,100"
+
+result = config.split(' ')[-1].split(',')
+print(result)
+print(result == ['1', '3', '10', '20', '30', '100'])
--- exercises/04_data_structures/task_4_4.py
+++ exercises/04_data_structures/task_4_4.py
@@ -24,3 +24,7 @@
 """
 
 vlans = [10, 20, 30, 1, 2, 100, 10, 30, 3, 4, 10]
+mid = set(vlans)
+result = sorted(mid)
+print(result)
+print(result == [1, 2, 3, 4, 10, 20, 30, 100])
